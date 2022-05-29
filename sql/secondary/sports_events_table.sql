acfa836bd231ede76cfd975cbfe8219fb0fa9ac7
inferrinizzard <inferrinizzard@gmail.com>
2022-05-29 13:01:22 -0700

    load sport events data into db

---
+++ sql/secondary/sports_events_table.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS sports_events (
+    id              SERIAL PRIMARY KEY,
+    game            VARCHAR(30) NOT NULL,
+    sport           VARCHAR(3) NOT NULL,
+    events          JSON NOT NULL,
+
+    -- CONSTRAINT fk_sports_events_sport
+    --     FOREIGN KEY(sport)
+    --     REFERENCES sports_detail(sport),
+    CONSTRAINT fk_sports_events_game
+        FOREIGN KEY(game)
+        REFERENCES games_detail(game)
+);
+
+COMMENT ON TABLE sports_events IS 'Main Sports table, holds all sports details';
+COMMENT ON COLUMN sports_events.game IS 'Games Key';
+COMMENT ON COLUMN sports_events.sport IS 'IOC Sport Code';
+COMMENT ON COLUMN sports_events.events IS 'JSON dict of events to event winners dict';
