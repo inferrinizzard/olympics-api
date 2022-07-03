0035d6a1b5021798eee728bce222fb500d80d8a5
inferrinizzard <inferrinizzard@gmail.com>
2022-07-03 11:23:08 -0700

    add gamesData route

---
+++ gamesData.d.ts
@@ -0,0 +1,13 @@
+export type GamesId = GamesDetailRow['game'];
+
+export interface GamesDetailRow extends Record<string, string | number> {
+	game: string;
+	year: number;
+	season: string;
+	title: string;
+	emblem: string;
+	host: string;
+	numAthletes: number;
+	startDate: string;
+	endDate: string;
+}
