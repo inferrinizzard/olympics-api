1b4d82bf9f87fb850dd4e123306074a972dc973d
inferrinizzard <inferrinizzard@gmail.com>
2022-07-12 16:08:47 -0700

    remove old routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,26 +1,14 @@
 import express from 'express';
 
-import countriesRouter from './countries.js';
-import gamesRouter from './games.js';
-import medalsRouter from './medals.js';
-import sportsRouter from './sports.js';
-import imagesRouter from './images.js';
-
 import { RegisterRoutes } from './routes.js';
 
 import swaggerRouter from './swagger.js';
 
 const router = express.Router();
 
-// router.get('/', (req, res) => res.send('Swagger UI'));
+router.get('/', (req, res) => res.redirect('/docs'));
 
 router.use('/docs', swaggerRouter);
-
-// router.use('/countries', countriesRouter);
-// router.use('/games', gamesRouter);
-// router.use('/medals', medalsRouter);
-// router.use('/sports', sportsRouter);
-// router.use('/images', imagesRouter);
 
 RegisterRoutes(router);
 
