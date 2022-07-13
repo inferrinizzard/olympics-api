5bc3f1b5b090cba5d1c536be852ea4d4ff201224
inferrinizzard <inferrinizzard@gmail.com>
2022-07-13 14:03:02 -0700

    update api return types for image endpoint

--- src/controllers/imageDataController.ts
+++ src/controllers/imageDataController.ts
@@ -1,22 +1,22 @@
 import { Controller, Get, Path, Route } from 'tsoa';
 
-import type { CountryDetailRow, CountryId } from '../models/countryData';
-import type { SportDetailRow, SportId } from '../models/sportsData';
-import type { GamesDetailRow, GamesId } from '../models/gamesData';
+import type { CountryFlagMap } from '../models/countryData';
+import type { SportsIconMap } from '../models/sportsData';
+import type { GamesEmblemMap } from '../models/gamesData';
 import { ImageDataService } from '../services/imageDataService.js';
 
 @Route('images')
 export class ImageDataController extends Controller {
 	@Get('countries')
-	public async getCountryFlags(): Promise<Record<CountryId, CountryDetailRow['flag']>> {
+	public async getCountryFlags(): Promise<CountryFlagMap> {
 		return new ImageDataService().getCountryFlags();
 	}
 	@Get('games')
-	public async getGamesEmblems(): Promise<Record<GamesId, GamesDetailRow['emblem']>> {
+	public async getGamesEmblems(): Promise<GamesEmblemMap> {
 		return new ImageDataService().getGamesEmblems();
 	}
 	@Get('sports')
-	public async getSportsLogos(): Promise<Record<SportId, SportDetailRow['logo']>> {
+	public async getSportsLogos(): Promise<SportsIconMap> {
 		return new ImageDataService().getSportsLogos();
 	}
 }
