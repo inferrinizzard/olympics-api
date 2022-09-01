ebc6f1ea7157f27ca0389b2bf9aaf92ecea07011
inferrinizzard <inferrinizzard@gmail.com>
2022-08-31 22:44:50 -0700

    split flag country into flag and icon

--- sql/primary/country_detail_table.sql
+++ sql/primary/country_detail_table.sql
@@ -3,6 +3,7 @@ DROP TABLE IF EXISTS country_detail CASCADE;
 CREATE TABLE IF NOT EXISTS country_detail (
     country     VARCHAR(3) PRIMARY KEY,
     name        VARCHAR(50) NOT NULL,
+    icon        TEXT NOT NULL,
     flag        TEXT NOT NULL,
     status      VARCHAR(10) NOT NULL
 );
@@ -10,5 +11,6 @@ CREATE TABLE IF NOT EXISTS country_detail (
 COMMENT ON TABLE country_detail IS 'Main Country table, holds all country details';
 COMMENT ON COLUMN country_detail.country IS 'Country Code';
 COMMENT ON COLUMN country_detail.name IS 'Country Name';
+COMMENT ON COLUMN country_detail.icon IS 'Country Icon URL';
 COMMENT ON COLUMN country_detail.flag IS 'Country Flag URL';
 COMMENT ON COLUMN country_detail.status IS 'If code is active, special, or historic';
