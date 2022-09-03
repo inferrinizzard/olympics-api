733c3188cbecf93f1afd5bd9d337ca3cd87a0346
inferrinizzard <inferrinizzard@gmail.com>
2022-09-03 15:45:41 -0700

    add country_attendance view from country_athletes

---
+++ sql/views/country_attendance_view.sql
@@ -0,0 +1,7 @@
+CREATE VIEW country_attendance AS
+SELECT
+    country,
+    ARRAY_AGG(game) AS games
+FROM country_athletes
+    CROSS JOIN JSON_OBJECT_KEYS(country_athletes) country
+GROUP BY country;
