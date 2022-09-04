bc4a5948238f4e42a578f80eb858ffee09fcad7e
inferrinizzard <inferrinizzard@gmail.com>
2022-09-03 22:22:42 -0700

    add medal_totals view

--- sql/views/country_medal_totals_view.sql
+++ sql/views/country_medal_totals_view.sql
@@ -1,13 +1,11 @@
-CREATE MATERIALIZED VIEW country_game_medals AS
+CREATE MATERIALIZED VIEW country_medal_totals AS
 SELECT
-    game,
     country,
     MAX(CASE WHEN gold IS NOT NULL THEN medals END) AS gold,
     MAX(CASE WHEN silver IS NOT NULL THEN medals END) AS silver,
     MAX(CASE WHEN bronze IS NOT NULL THEN medals END) AS bronze
 FROM (
     SELECT
-        game,
         COALESCE(gold, silver, bronze) AS country,
         gold,
         silver,
@@ -15,16 +13,14 @@ FROM (
         COUNT(*) AS medals
     FROM (
         SELECT
-            game,
             UNNEST(gold) AS gold,
             UNNEST(silver) AS silver,
             UNNEST(bronze) AS bronze
         FROM sports_events
     ) unnested
     GROUP BY
-        game,
         GROUPING SETS( (gold), (silver), (bronze) )
 ) counted
 WHERE country IS NOT NULL
-GROUP BY game, country
+GROUP BY country
 ;
