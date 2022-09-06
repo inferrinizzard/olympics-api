a9d563dbf43facbde6794024196efb3de3d844bb
inferrinizzard <inferrinizzard@gmail.com>
2022-09-06 09:04:01 -0700

    remove comments on views

--- sql/views/country_attendance_view.sql
+++ sql/views/country_attendance_view.sql
@@ -6,6 +6,6 @@ FROM country_athletes
     CROSS JOIN JSON_OBJECT_KEYS(country_athletes) country
 GROUP BY country;
 
-COMMENT ON TABLE country_attendance IS 'Shows which games each country has attended';
-COMMENT ON COLUMN country_attendance.country IS 'Country Code';
-COMMENT ON COLUMN country_attendance.games IS 'List of games that country did attend';
+-- COMMENT ON TABLE country_attendance IS 'Shows which games each country has attended';
+-- COMMENT ON COLUMN country_attendance.country IS 'Country Code';
+-- COMMENT ON COLUMN country_attendance.games IS 'List of games that country did attend';
--- sql/views/country_medal_totals_view.sql
+++ sql/views/country_medal_totals_view.sql
@@ -10,9 +10,9 @@ JOIN games_detail
     ON country_game_medals.game = games_detail.game
 GROUP BY country, season;
 
-COMMENT ON TABLE country_medal_totals IS 'View with the number of medals per country and season';
-COMMENT ON COLUMN country_medal_totals.country IS 'Country Code';
-COMMENT ON COLUMN country_medal_totals.season IS 'Type of games (summer|winter)';
-COMMENT ON COLUMN country_medal_totals.gold IS 'Number of gold medals';
-COMMENT ON COLUMN country_medal_totals.silver IS 'Number of silver medals';
-COMMENT ON COLUMN country_medal_totals.bronze IS 'Number of bronze medals';
+-- COMMENT ON TABLE country_medal_totals IS 'View with the number of medals per country and season';
+-- COMMENT ON COLUMN country_medal_totals.country IS 'Country Code';
+-- COMMENT ON COLUMN country_medal_totals.season IS 'Type of games (summer|winter)';
+-- COMMENT ON COLUMN country_medal_totals.gold IS 'Number of gold medals';
+-- COMMENT ON COLUMN country_medal_totals.silver IS 'Number of silver medals';
+-- COMMENT ON COLUMN country_medal_totals.bronze IS 'Number of bronze medals';
