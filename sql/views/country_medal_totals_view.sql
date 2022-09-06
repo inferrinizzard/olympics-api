286941642ed610d2313f0d3350db7ec6a4a949e3
inferrinizzard <inferrinizzard@gmail.com>
2022-09-06 09:03:53 -0700

    update country_medal_totals to view on country_game_medals

--- sql/views/country_medal_totals_view.sql
+++ sql/views/country_medal_totals_view.sql
@@ -2,34 +2,13 @@ CREATE MATERIALIZED VIEW IF NOT EXISTS country_medal_totals AS
 SELECT
     country,
     season,
-    MAX(CASE WHEN gold IS NOT NULL THEN medals END) AS gold,
-    MAX(CASE WHEN silver IS NOT NULL THEN medals END) AS silver,
-    MAX(CASE WHEN bronze IS NOT NULL THEN medals END) AS bronze
-FROM (
-    SELECT
-        season,
-        COALESCE(gold, silver, bronze) AS country,
-        gold,
-        silver,
-        bronze,
-        COUNT(*) AS medals
-    FROM (
-        SELECT
-            games_detail.season AS season,
-            UNNEST(gold) AS gold,
-            UNNEST(silver) AS silver,
-            UNNEST(bronze) AS bronze
-        FROM sports_events
-        JOIN games_detail
-            ON sports_events.game = games_detail.game
-    ) unnested
-    GROUP BY
-        season,
-        GROUPING SETS( (gold), (silver), (bronze) )
-) counted
-WHERE country IS NOT NULL
-GROUP BY country, season
-;
+    SUM(gold) AS gold,
+    SUM(silver) AS silver,
+    SUM(bronze) AS bronze
+FROM country_game_medals
+JOIN games_detail
+    ON country_game_medals.game = games_detail.game
+GROUP BY country, season;
 
 COMMENT ON TABLE country_medal_totals IS 'View with the number of medals per country and season';
 COMMENT ON COLUMN country_medal_totals.country IS 'Country Code';
