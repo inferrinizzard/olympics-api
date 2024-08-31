d9f567db555934824ec2c825e0971ad1d11b1c64
inferrinizzard <inferrinizzard@gmail.com>
2024-08-31 13:02:38 -0700

    manually extract motto text

--- scripts/scraper/wikipedia/fetchGamesData.ts
+++ scripts/scraper/wikipedia/fetchGamesData.ts
@@ -1,7 +1,14 @@
 import wiki from 'wikipedia';
+import { JSDOM } from 'jsdom';
 import anyDateParser from 'any-date-parser';
 
 import type { PartialGamesList } from '../../../src/models';
+
+const getInfoboxElement = (html: string) => {
+  const dom = new JSDOM(html);
+  const infobox = dom.window.document.querySelector('table.infobox');
+  return infobox as HTMLTableElement;
+};
 
 const extractValue = (
   infoboxMap: Record<string, string>,
@@ -29,17 +36,32 @@ const dateProcessor = (value: string, year: number) => {
   }
 };
 
+const extractMottoFromInfoBox = (infobox: HTMLTableElement) => {
+  const mottoRow = [...infobox.rows].find(
+    (row) => row.cells[0].textContent?.toLowerCase() === 'motto'
+  );
+
+  const mottoValueCell = mottoRow?.cells[1];
+  const mottoText = [
+    ...(mottoValueCell?.querySelectorAll('span[lang]') || []),
+    ...(mottoValueCell?.getElementsByTagName('i') || []),
+  ].flatMap((element) => element.textContent ?? []);
+
+  return mottoText.length ? mottoText : undefined;
+};
+
 export const readGamesInfoBoxFromPage = async (games: PartialGamesList) => {
   const gamesPage = await wiki.page(games.pageName, {
     preload: true,
-    fields: ['infobox', 'images'],
+    fields: ['infobox', 'images', 'html'],
   });
   const gamesInfobox = gamesPage._infobox;
+  const gamesInfoBoxElement = getInfoboxElement(gamesPage._html);
 
   const image = gamesPage._images.find(
     (image) => image.title.includes('logo') || image.title.includes('poster')
   )?.url;
-  const motto = gamesInfobox.motto;
+  const motto = extractMottoFromInfoBox(gamesInfoBoxElement);
 
   const numAthletes = extractValue(
     gamesInfobox,
