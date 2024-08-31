a5cec090e34cb6a50d3ed02de6d7a076f6e8b02f
inferrinizzard <inferrinizzard@gmail.com>
2024-08-31 10:40:49 -0700

    scrape games details to file

--- scripts/scraper/wikipedia/index.ts
+++ scripts/scraper/wikipedia/index.ts
@@ -1,12 +1,28 @@
-import { writeFileSync } from 'fs';
+import { writeFileSync, existsSync } from 'fs';
 import { fetchGamesList } from './fetchGamesList';
+
+import gamesList from '../../../json/gamesList.json';
+import { readGamesInfoBoxFromPage } from './fetchGamesData';
+
+const GAMES_LIST_JSON_PATH = './json/gamesList.json';
+const GAMES_DETAIL_JSON_PATH = './json/gamesDetail2.json';
 
 const main = async () => {
   console.log('START');
 
-  console.log('GET INITIAL GAMES PAGES');
-  const gamesList = await fetchGamesList();
-  writeFileSync('./json/gamesList.json', JSON.stringify(gamesList, null, 2));
+  console.log('GET INITIAL GAMES LIST');
+  if (existsSync(GAMES_LIST_JSON_PATH) && gamesList.length) {
+    console.log('Games List already exists, skipping fetch');
+  } else {
+    const newGamesList = await fetchGamesList();
+    writeFileSync(GAMES_LIST_JSON_PATH, JSON.stringify(newGamesList, null, 2));
+  }
+
+  console.log('GET GAMES DETAILS');
+  const gamesDetail = await Promise.all(
+    gamesList.map(readGamesInfoBoxFromPage)
+  );
+  writeFileSync(GAMES_DETAIL_JSON_PATH, JSON.stringify(gamesDetail, null, 2));
 };
 
 await main();
