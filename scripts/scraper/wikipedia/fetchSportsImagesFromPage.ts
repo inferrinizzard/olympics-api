60e86850ae5a6487743fd1fd4691d2f8870c7bb7
inferrinizzard <inferrinizzard@gmail.com>
2024-09-01 11:19:51 -0700

    fix and stream to csv instead

--- scripts/scraper/wikipedia/fetchSportsImagesFromPage.ts
+++ scripts/scraper/wikipedia/fetchSportsImagesFromPage.ts
@@ -1,8 +1,9 @@
-import { writeFileSync } from 'fs';
+import { createWriteStream, writeFileSync } from 'fs';
 import wiki from 'wikipedia';
 
 import sportsList from '../../../json/sportsDetail2.json';
 import sportsPages from '../../../json/gamesSportsLinksMap.json';
+
 import { extractImageFromInfobox, getInfoboxElement } from './infobox';
 
 const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
@@ -13,9 +14,12 @@ const matchSportMap = sportsList.reduce((map, sport) => {
   return map;
 }, {} as Record<string, string>);
 
-let fails = 0;
+let consecutiveFails = 0;
 const noMatch = {} as Record<string, string[]>;
 const gamesSportsImages = {} as Record<string, Record<string, string>>;
+
+const imageStream = createWriteStream('./json/gamesSportsImages.csv');
+const missingStream = createWriteStream('./json/missingGamesSportsImages.csv');
 
 for (const [games, pageList] of Object.entries(sportsPages)) {
   console.log(games);
@@ -30,7 +34,8 @@ for (const [games, pageList] of Object.entries(sportsPages)) {
 
     const sportCode = matchSportMap[matchSportName];
     if (!sportCode) {
-      noMatch[games].push(page);
+      // noMatch[games].push(page);
+      missingStream.write(`${games}, ${page}\n`);
       continue;
     }
 
@@ -42,27 +47,33 @@ for (const [games, pageList] of Object.entries(sportsPages)) {
       const infobox = getInfoboxElement(sportsPage._html);
       const sportsImage = extractImageFromInfobox(infobox);
 
-      gamesSportsImages[games][sportCode] = sportsImage;
+      // gamesSportsImages[games][sportCode] = sportsImage;
+      imageStream.write(`${games}, ${sportCode}, ${sportsImage}\n`);
+      consecutiveFails = 0;
     } catch {
-      fails++;
+      consecutiveFails++;
       console.log('FAILED', games, page);
-      noMatch[games].push('FAILED:' + page);
+      missingStream.write(`${games}, FAILED:${sportCode}\n`);
+      // noMatch[games].push('FAILED:' + page);
 
-      if (fails > 3) {
+      if (consecutiveFails > 3) {
         break;
       }
     }
   }
 
-  if (fails > 3) {
+  if (consecutiveFails > 3) {
     break;
   }
 
   await delay(10000);
 }
 
-writeFileSync('./json/noMatch.json', JSON.stringify(noMatch, null, 2));
-writeFileSync(
-  './json/gamesSportsImages.json',
-  JSON.stringify(gamesSportsImages, null, 2)
-);
+imageStream.close();
+missingStream.close();
+
+// writeFileSync('./json/noMatch.json', JSON.stringify(noMatch, null, 2));
+// writeFileSync(
+//   './json/gamesSportsImages.json',
+//   JSON.stringify(gamesSportsImages, null, 2)
+// );
