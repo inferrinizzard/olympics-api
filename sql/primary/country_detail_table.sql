fe834b512b6a6515e7d0f0138c601c08f0300125
inferrinizzard <inferrinizzard@gmail.com>
2024-09-05 23:15:49 -0700

    update sql for primary table creation

--- sql/primary/country_detail_table.sql
+++ sql/primary/country_detail_table.sql
@@ -1,12 +1,12 @@
 CREATE TABLE IF NOT EXISTS country_detail (
-    country     VARCHAR(3) PRIMARY KEY,
-    name        VARCHAR(50) NOT NULL,
-    flag        TEXT NOT NULL,
+    code        VARCHAR(3) PRIMARY KEY,
+    name        TEXT NOT NULL,
+    page_name   TEXT NOT NULL,
     status      VARCHAR(10) NOT NULL
 );
 
 COMMENT ON TABLE country_detail IS 'Main Country table, holds all country details';
-COMMENT ON COLUMN country_detail.country IS 'Country Code';
+COMMENT ON COLUMN country_detail.code IS 'Country Code';
 COMMENT ON COLUMN country_detail.name IS 'Country Name';
-COMMENT ON COLUMN country_detail.flag IS 'Country Flag URL';
+COMMENT ON COLUMN country_detail.page_name IS 'Country Wikipedia page name';
 COMMENT ON COLUMN country_detail.status IS 'If code is active, special, or historic';
--- sql/primary/sports_detail_table.sql
+++ sql/primary/sports_detail_table.sql
@@ -1,10 +1,16 @@
 CREATE TABLE IF NOT EXISTS sports_detail (
-    sport       VARCHAR(3) PRIMARY KEY,
+    code        VARCHAR(5) PRIMARY KEY,
     name        VARCHAR(30) NOT NULL,
-    icon        TEXT NOT NULL
+    category    VARCHAR(15) NOT NULL,
+    parent      VARCHAR(5),
+    status      VARCHAR(15),
+    season      VARCHAR(6)
 );
 
 COMMENT ON TABLE sports_detail IS 'Main Sports table, holds all sports details';
-COMMENT ON COLUMN sports_detail.sport IS 'IOC Sport Code';
+COMMENT ON COLUMN sports_detail.code IS 'IOC/IPC Sport Code';
 COMMENT ON COLUMN sports_detail.name IS 'Sport Name';
-COMMENT ON COLUMN sports_detail.icon IS 'Sport Icon URL';
+COMMENT ON COLUMN sports_detail.category IS 'Sport or Discipline';
+COMMENT ON COLUMN sports_detail.parent IS 'Parent sport, if is Discipline';
+COMMENT ON COLUMN sports_detail.status IS 'If sport is active, other, unrecognised, NULL';
+COMMENT ON COLUMN sports_detail.season IS 'Sport season, if active';
