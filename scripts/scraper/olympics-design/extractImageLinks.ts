9a0e4352bcd6a41723ee15a1e484e9a02aaec3c5
inferrinizzard <inferrinizzard@gmail.com>
2024-09-07 16:35:48 -0700

    map olympics designs images to sport

--- scripts/scraper/olympics-design/extractImageLinks.ts
+++ scripts/scraper/olympics-design/extractImageLinks.ts
@@ -1,4 +1,4 @@
-import { writeFileSync } from 'fs';
+import { writeFileSync } from 'node:fs';
 import { JSDOM } from 'jsdom';
 
 import olympicsDesignLinks from '@/json/partial/olympicDesignLinks.json';
@@ -6,21 +6,38 @@ import olympicsDesignLinks from '@/json/partial/olympicDesignLinks.json';
 const OLYMPIC_DESIGN_SPORTS_PICTOGRAMS_LINKS_JSON_PATH =
   './json/partial/olympicDesignSportsPictograms.json';
 
-const gamesSportsPictogramsLinks: Record<string, string[]> = {};
+const gamesSportsPictogramsLinks: Record<string, unknown> = {};
 
 const getImageLinksForPage = async (link: string) => {
   const response = await fetch(link);
   const document = new JSDOM(await response.text()).window.document;
 
-  const aImageLinks = [
-    ...document.querySelectorAll('a[rel="lightbox"] > img'),
-  ].flatMap((i) => i.getAttribute('src') ?? []);
+  const imageBlocks = [
+    ...document.getElementsByClassName('cc-m-hgrid-column'),
+  ] as HTMLDivElement[];
+  const unknown = imageBlocks
+    .filter((el) => !el.querySelector('p')?.textContent?.trim())
+    .flatMap((div) => div.querySelector('img')?.getAttribute('src') ?? [])
+    .filter((url) => !url.includes('none'));
 
-  const figureImageLinks = [
-    ...document.querySelectorAll('figure > img'),
-  ].flatMap((i) => i.getAttribute('src') ?? []);
+  const known = imageBlocks.filter((el) => el.querySelector('p')?.textContent);
+  const imageMap = known.reduce((acc, div) => {
+    const sport = div.querySelector('p')?.textContent?.trim() ?? '';
+    acc[sport] = div.querySelector('img')?.getAttribute('src') ?? '';
+    return acc;
+  }, {} as Record<string, string>);
 
-  return [...aImageLinks, ...figureImageLinks];
+  // const aImageLinks = [
+  //   ...document.querySelectorAll('a[rel="lightbox"] > img'),
+  // ].flatMap((i) => i.getAttribute('src') ?? []);
+
+  // const figureImageLinks = [
+  //   ...document.querySelectorAll('figure > img'),
+  // ].flatMap((i) => i.getAttribute('src') ?? []);
+
+  // return [...aImageLinks, ...figureImageLinks];
+
+  return { unknown, known: imageMap };
 };
 
 for (const link of olympicsDesignLinks.main) {
