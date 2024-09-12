da0e25076cca34a65cc169d30e75b35464043a00
inferrinizzard <inferrinizzard@gmail.com>
2024-09-12 07:52:01 -0700

    add basic scraper for sports events

--- scripts/scraper/utils/getDocument.ts
+++ scripts/scraper/utils/getDocument.ts
@@ -2,6 +2,11 @@ import { JSDOM } from 'jsdom';
 
 export const getDocument = async (url: string) => {
   const response = await fetch(url);
+
+  if (response.status !== 200) {
+    return response.status;
+  }
+
   const document = new JSDOM(await response.text()).window.document;
   return document;
 };
